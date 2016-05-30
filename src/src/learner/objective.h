87fe89600650c867571ac6e7d6187b90c3c1ed1e
rnorm <zhang.nl@outlook.com>
2016-05-30 11:05:09 -0400

    Update objective.h
    add mape objective

--- src/src/learner/objective.h
+++ src/src/learner/objective.h
@@ -72,6 +72,7 @@ namespace learner {
 inline IObjFunction* CreateObjFunction(const char *name) {
   using namespace std;
   if (!strcmp("reg:linear", name)) return new RegLossObj(LossType::kLinearSquare);
+  if (!strcmp("reg:mape", name)) return new RegLossObj(LossType::kLinearMape);
   if (!strcmp("reg:logistic", name)) return new RegLossObj(LossType::kLogisticNeglik);
   if (!strcmp("binary:logistic", name)) return new RegLossObj(LossType::kLogisticClassify);
   if (!strcmp("binary:logitraw", name)) return new RegLossObj(LossType::kLogisticRaw);
